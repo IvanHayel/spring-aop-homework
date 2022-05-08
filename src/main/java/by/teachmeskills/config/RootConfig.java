b7652f8248fa3ce41e6959886a4a50cc9cdef15c
Hayel <ivanforwork@yandex.ru>
2022-05-08 18:43:53 +0300

    Configuration class added.

---
+++ src/main/java/by/teachmeskills/config/RootConfig.java
@@ -0,0 +1,11 @@
+package by.teachmeskills.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = "by.teachmeskills")
+@EnableAspectJAutoProxy
+public class RootConfig {
+}
